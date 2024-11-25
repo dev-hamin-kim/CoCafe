6db0ddd96ffe0e89c7d6bfdffe7da9e6f558ab1e
Hamin Kim <dev.hamin.kim@gmail.com>
2024-11-25 17:30:09 +0900

    [add] Assets에 메뉴 아이콘 추가 및 Menu의 MenuList에 파일명 삽입

--- CoCafe/Models/Menu.swift
+++ CoCafe/Models/Menu.swift
@@ -17,16 +17,16 @@ struct Item {
     
     
     static var MenuList: [Item] = [
-        Item(name: "아이스 아메리카노", category: .Coffee, price: 4500, icon: ""),
-        Item(name: "아이스 카페라떼", category: .Coffee, price: 4900, icon: ""),
-        Item(name: "코난 라떼", category: .Coffee, price: 5000, icon: ""),
+        Item(name: "아이스 아메리카노", category: .Coffee, price: 4500, icon: "IcedAmericano"),
+        Item(name: "아이스 카페라떼", category: .Coffee, price: 4900, icon: "IcedCafeLatte"),
+        Item(name: "코난 라떼", category: .Coffee, price: 5000, icon: "ConanLatte"),
         
-        Item(name: "초코 아이스크림 라떼", category: .NonCoffee, price: 5500, icon: ""),
-        Item(name: "스트로베리 아이스크림 라떼", category: .NonCoffee, price: 5500, icon: ""),
-        Item(name: "캐모마일 티", category: .NonCoffee, price: 4500, icon: ""),
+        Item(name: "초코 아이스크림 라떼", category: .NonCoffee, price: 5500, icon: "ChocolateIcecreamLatte"),
+        Item(name: "스트로베리 아이스크림 라떼", category: .NonCoffee, price: 5500, icon: "StrawberryIcecreamLatte"),
+        Item(name: "캐모마일 티", category: .NonCoffee, price: 4500, icon: "IcedChamomileTea"),
         
-        Item(name: "크로칸 슈", category: .Dessert, price: 5000, icon: ""),
-        Item(name: "치즈케이크", category: .Dessert, price: 5500, icon: ""),
+        Item(name: "크로칸 슈", category: .Dessert, price: 5000, icon: "CroquantChoux"),
+        Item(name: "치즈케이크", category: .Dessert, price: 5500, icon: "CheeseCake"),
     ]
 }
 
