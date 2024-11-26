658a7bf6a0ac1858b53c3476360ea40853010907
zzangmmz <leemj036@gmail.com>
2024-11-26 19:53:26 +0900

    [feat] UITableViewDataSource, UITableViewDelegate 프로토콜 채택 후 임시로 Item의 MenuList 출력

--- CoCafe/Views/CartTableView.swift
+++ CoCafe/Views/CartTableView.swift
@@ -6,12 +6,27 @@
 //
 import UIKit
 
-final class CartTableView: UIView {
+final class CartTableView: UIView, UITableViewDataSource, UITableViewDelegate {
+    let orders = Item.MenuList.map { item in
+        Order(item: item, count: Int.random(in: 1 ... 20))
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: .zero)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return orders.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CartCell", for: indexPath) as! CartTableViewCell
+        cell.configureCellData(order: orders[indexPath.row])
+        cell.selectionStyle = .none
+        return cell
     }
 }
