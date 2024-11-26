f2d4378b678ca0b10d023e05db090a1bc39af2ec
zzangmmz <leemj036@gmail.com>
2024-11-26 19:51:20 +0900

    [feat] 테이블뷰 올릴 CartTableView 구성

--- CoCafe/Views/CartTableView.swift
+++ CoCafe/Views/CartTableView.swift
@@ -4,3 +4,14 @@
 //
 //  Created by 이명지 on 11/25/24.
 //
+import UIKit
+
+final class CartTableView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
