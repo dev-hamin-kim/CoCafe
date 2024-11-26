b1b96719f8e9aee1e520818d5d59aecd83968a41
zzangmmz <leemj036@gmail.com>
2024-11-26 18:43:04 +0900

    [refactor] cellStackView의 trailingAnchor 속성 constant -20 -> -18로 수정

--- CoCafe/Views/CartTableViewCell.swift
+++ CoCafe/Views/CartTableViewCell.swift
@@ -97,7 +97,7 @@ final class CartTableViewCell: UITableViewCell {
             menuNameLabel.widthAnchor.constraint(equalToConstant: 130),
             
             cellStackView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 26),
-            cellStackView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -20),
+            cellStackView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -18),
             cellStackView.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 10),
             cellStackView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -10)
         ])
