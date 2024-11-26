3421a35b1e235db752756e87de8d4fc50eaf1549
soy <soysauce1223@naver.com>
2024-11-26 11:38:18 +0900

    [feat] UISegmentedControl 생성

--- CoCafe/Views/MenuCategoryView.swift
+++ CoCafe/Views/MenuCategoryView.swift
@@ -5,3 +5,15 @@
 //  Created by seohuibaek on 11/25/24.
 //
 
+import UIKit
+
+class MenuCategoryView: UIView {
+    private let menuSegmentedControl: UISegmentedControl = {
+        let items = ["Coffee", "Non-Coffee", "Dessert"]
+        let control = UISegmentedControl(items: items)
+        control.selectedSegmentIndex = 0  // 기본 선택값
+        control.backgroundColor = .white
+        control.selectedSegmentTintColor = .systemBlue  // 선택된 세그먼트 색상
+        return control
+    }()
+}
