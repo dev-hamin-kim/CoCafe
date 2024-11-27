b487431c9c2f334b7c7f66286496f4e63d153774
soy <soysauce1223@naver.com>
2024-11-27 14:14:29 +0900

    [update] MenuCategoryViewController 상단 로고 View와 CategoryView StackView로 병합 (임시로 설정 해당 파일 삭제 예정)

--- CoCafe/Controllers/MenuCategoryViewController.swift
+++ CoCafe/Controllers/MenuCategoryViewController.swift
@@ -9,15 +9,36 @@ import UIKit
 
 class MenuCategoryViewController: UIViewController {
     
-    var menuCategoryView : MenuCategoryView!
-
-    override func loadView() {
-        menuCategoryView = MenuCategoryView()
-        view = menuCategoryView
-    }
+    var menuCategoryView: MenuCategoryView!
+    var topLogoView: TopLogoView!
+    var stackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+    }
+    
+    private func setupViews() {
+        topLogoView = TopLogoView()
+        menuCategoryView = MenuCategoryView()
+        
+        stackView = UIStackView(arrangedSubviews: [topLogoView, menuCategoryView])
+        stackView.axis = .vertical
+        stackView.spacing = 10      // 뷰 사이의 간격
+        stackView.alignment = .fill // 자식 뷰 가로 크기 채움
+        stackView.distribution = .fill // 자식 뷰 비율에 따라 채우기
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        topLogoView.heightAnchor.constraint(equalToConstant: 45).isActive = true
     }
 }
 
