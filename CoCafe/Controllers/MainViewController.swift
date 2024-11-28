b0f9ed5a1b137d7e5f9f03dfd72f02fb4e5fbe0b
zzangmmz <leemj036@gmail.com>
2024-11-28 18:05:45 +0900

    [refactor] delegate 프로토콜 채택 extension으로 분리

--- CoCafe/Controllers/MainViewController.swift
+++ CoCafe/Controllers/MainViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MainViewController: UIViewController, MenuCategoryViewDelegate, CoffeeMenuViewDelegate {
+class MainViewController: UIViewController {
     
     var stackView: CoCafeTopStackView! //스택뷰로 View 병합
     var coCafeTopView: CoCafeTopView! //addSubView로 View 병합
@@ -20,38 +20,23 @@ class MainViewController: UIViewController, MenuCategoryViewDelegate, CoffeeMenu
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        // stackView 사용하는 경우
         stackView = CoCafeTopStackView()
         view = stackView
         
-        //delegate nil 되는 이슈 발생 -> menuCategoryView.delegate = self 말고 아래와 같이 선언해주어야 함..
         stackView.menuCategoryView.delegate = self
         stackView.coffeeMenuView.updateMenuItems(for: .Coffee) // 초기 데이터 설정
         
         stackView.coffeeMenuView.delegate = self  // delegate 설정
-        
-        // addsubView 사용하는 경우
-        /*coCafeTopView = CoCafeTopView()
-         view = coCafeTopView
-         
-         
-         //delegate nil 되는 이슈 발생 -> menuCategoryView.delegate = self 말고 아래와 같이 선언해주어야 함..
-         coCafeTopView.menuCategoryView.delegate = self
-         coCafeTopView.coffeeMenuView.updateMenuItems(for: .Coffee) // 초기 데이터 설정*/
     }
-    
+}
+
+extension MainViewController: MenuCategoryViewDelegate {
     func categoryChanged(to category: Category) {
-        // stackView 사용하는 경우
         stackView.coffeeMenuView.updateMenuItems(for: category)
-        
-        // addsubView 사용하는 경우
-        //coCafeTopView.coffeeMenuView.updateMenuItems(for: category)
-        
-        //print(category)
-        //print("Category Changed to: \(category)")
     }
-    
+}
+
+extension MainViewController: CoffeeMenuViewDelegate {
     func didUpdateItem(_ item: Item) {
         //cart.addToCart(item: item)
         print(item)
