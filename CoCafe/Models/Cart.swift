f5df76e0e800eed754edb99c254fb2e621e3c8eb
zzangmmz <leemj036@gmail.com>
2024-11-28 22:00:23 +0900

    [fix] 동일 음료 클릭 시 수량 추가 안되는 오류 수정

--- CoCafe/Models/Cart.swift
+++ CoCafe/Models/Cart.swift
@@ -20,7 +20,7 @@ struct Cart {
         return result
     }
     
-
+    
     /// Cart 내 Order들의 총 갯수를 출력합니다.
     func totalQuantity() -> Int {
         var result: Int = 0
@@ -32,29 +32,28 @@ struct Cart {
         return result
     }
     
-
+    
     /// Cart 내 row번째 항목을 삭제합니다. (선택 삭제)
     mutating func deleteOrder(in row: Int) {
         orders.remove(at: row)
     }
     
-
+    
     /// Cart안의 모든 orders를 없앱니다 (전체 취소)
     mutating func clearCart() {
         orders = []
     }
     
-
+    
     /// 옵션까지 동일한 item이 Cart의 주문내역에 있다면, 같은 item의 count을 1 늘려주고,
     /// 주문내역에 동일한 item이 없다면 해당 item을 count 1으로 넣어줍니다.
     mutating func addToCart(item: Item) {
-        let existingOrder = orders.first(where: { $0.item == item })
-
-        guard var existingOrder else { return orders.append(Order(item: item, count: 1)) }
-
-        
-        existingOrder.addOne()
-        
+        let hasSameItem = orders.contains(where: { $0.item == item })
+        guard hasSameItem else { return orders.append(Order(item: item, count: 1)) }
+        for index in 0..<orders.count {
+            if orders[index].item == item {
+                orders[index].addOne()
+            }
+        }
     }
-    
 }
