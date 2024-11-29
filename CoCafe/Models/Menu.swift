d90f7821f4d24fe46937900127e9fc33120a2797
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2024-11-29 09:47:19 +0900

    [update] Assets에 추가된 이미지에 따라 Menu 구조체의 MenuList에 Item으로 추가함.

--- CoCafe/Models/Menu.swift
+++ CoCafe/Models/Menu.swift
@@ -37,16 +37,26 @@ struct Item: Equatable {
     }
     
     static var MenuList: [Item] = [
-        Item(name: "아이스 아메리카노", category: .Coffee, price: 4500, icon: "IcedAmericano"),
+        Item(name: "아이스 아메리카노", category: .Coffee, price: 4000, icon: "IcedAmericano"),
         Item(name: "아이스 카페라떼", category: .Coffee, price: 4900, icon: "IcedCafeLatte"),
-        Item(name: "코난 라떼", category: .Coffee, price: 5000, icon: "ConanLatte"),
+        Item(name: "코난 라떼", category: .Coffee, price: 6000, icon: "ConanLatte"),
+        Item(name: "카페 라떼", category: .Coffee, price: 4500, icon: "CafeLatte"),
+        Item(name: "카페 모카", category: .Coffee, price: 5000, icon: "CafeMocha"),
+        Item(name: "아이스 카페모카", category: .Coffee, price: 5500, icon: "IcedCafeMocha"),
         
         Item(name: "초코 아이스크림 라떼", category: .NonCoffee, price: 5500, icon: "ChocolateIcecreamLatte"),
         Item(name: "딸기 아이스크림 라떼", category: .NonCoffee, price: 5500, icon: "StrawberryIcecreamLatte"),
         Item(name: "캐모마일 티", category: .NonCoffee, price: 4500, icon: "IcedChamomileTea"),
+        Item(name: "핫 코코아", category: .NonCoffee, price: 5000, icon: "HotCocoa"),
+        Item(name: "아이스 코코아", category: .NonCoffee, price: 5000, icon: "IcedCocoa"),
+        Item(name: "말차 라떼", category: .NonCoffee, price: 5000, icon: "MatchaLatte"),
         
-        Item(name: "크로칸 슈", category: .Dessert, price: 5000, icon: "CroquantChoux"),
+        Item(name: "크로칸 슈", category: .Dessert, price: 3500, icon: "CroquantChoux"),
         Item(name: "치즈케이크", category: .Dessert, price: 5500, icon: "CheeseCake"),
+        Item(name: "초콜릿 헤이즐넛 케이크", category: .Dessert, price: 5000, icon: "ChocolateHazelnutCake"),
+        Item(name: "딸기 케이크", category: .Dessert, price: 5000, icon: "StrawberryCake"),
+        Item(name: "에그타르트", category: .Dessert, price: 3000, icon: "EggTart"),
+        Item(name: "티라미수", category: .Dessert, price: 4500, icon: "Tiramisu")
     ]
 }
 
