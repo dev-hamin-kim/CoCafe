3710cb762993b4bbac30ada215d7c8eb1928b441
zzangmmz <leemj036@gmail.com>
2024-11-29 13:16:51 +0900

    [refactor] MainViewController extension 부분을 CartTotalView 파일 -> MainViewControlle 파일로 이동

--- CoCafe/Controllers/MainViewController.swift
+++ CoCafe/Controllers/MainViewController.swift
@@ -36,6 +36,43 @@ extension MainViewController: CoffeeMenuViewDelegate {
     }
 }
 
+extension MainViewController: CartTotalViewDelegate {
+    func showOrderCancelAlert() {
+        let alert = UIAlertController(title: "메뉴 전체 취소", message: "전체 메뉴를 취소하시겠습니까?", preferredStyle: .alert)
+        let success = UIAlertAction(title: "확인", style: .default) { action in
+            Cart.shared.clearCart()
+        }
+        let cancel = UIAlertAction(title: "취소", style: .default)
+        
+        alert.addAction(success)
+        alert.addAction(cancel)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func showPayAlert() {
+        let alert = UIAlertController(title: "결제 하시겠습니까?", message: "", preferredStyle: .alert)
+        let success = UIAlertAction(title: "확인", style: .default) { [weak self] action in
+            self?.showCompletePayAlert()
+        }
+        let cancel = UIAlertAction(title: "취소", style: .default)
+        
+        alert.addAction(success)
+        alert.addAction(cancel)
+
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func showCompletePayAlert() {
+        let alert = UIAlertController(title: "결제 완료", message: "", preferredStyle: .alert)
+        let success  = UIAlertAction(title: "확인", style: .default)
+        
+        alert.addAction(success)
+
+        present(alert, animated: true, completion: nil)
+    }
+}
+
 @available(iOS 17.0, *)
 #Preview {
     MainViewController()
